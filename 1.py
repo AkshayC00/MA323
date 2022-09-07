da6003b0aba0403e80a76a79a9f7d4476f367134
AkshayC00 <76867614+AkshayC00@users.noreply.github.com>
2022-09-07 19:27:12 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,113 +1,39 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
 
-N = 10000
-
-# Creating a list to store sample
-seq = []
-
-# defining the function f(x)
-def f(x):
-    return 20*x*(1-x)**3
-
-# defining the function g(x)
-def g(x):
-    return 2*x
-
-# defining the inverse of CDF G(x)
-def G_in(x):
-    return np.sqrt(x)
-
-# list to store num of iterations to generate a variable from distribution f(x)
-iter = [0]*N
-
-# Function for generating 10000 random variables
-def generate_rv(c):
+# Function to generate random sample
+def genSample(m):
+    # List to store the sample
     seq = []
-    for i in range(N):
-        while 1:
-            # generate u from U(0,1)
-            u = np.random.uniform(0,1)
-
-            # generate random variable X
-            x = G_in(u)
-
-            # Compute probability of acceptance
-            p = f(x)/(c*g(x))
-            p_x = np.random.uniform(0,1) 
-            if p_x<=p:
-                iter[i]+=1
-                seq.append(x)
-                break
-            else:
-                iter[i]+=1
-                
-    # computing sample mean
-    sample_mean = np.mean(seq)
-    print("Sample mean = ", sample_mean)
-
-    print("Expectation of f(x) = ", 1/3)
-
-    # Calculating approx value of P(0.25<=X<=0.75)
-    p1 = np.sum([x<=0.75 and x>=0.25 for x in seq])/len(seq)
-    print("Estimated value of P(0.25<=X<=0.75): ", p1)
-
-    # Exact value of probability
-    print("Exact value of P(0.25<=X<=0.75): ", 79/128)
-
-    # Estimated average number of iterations required
-    avg_it = np.mean(iter)
-    print("Average number of iterations = ", avg_it)
-
-    # Average number of iterations
-    print("Actual Average number of iterations = ", c)
+    for i in range(m):
+        # generate U from U(0,1)
+        u = np.random.random()
+        # generate Y_i
+        y = np.exp(np.sqrt(u))
+        # append in the sequence
+        seq.append(y)
 
     return seq
 
-# Generating seq using the smallest possible value of c
-seq = generate_rv(c=10)
+# Function to calculate the confidence interval
+def confInt(confidence, seq):
+    m = len(seq)
+    mean = np.sum(seq)/m
+    # Compute value of delta
+    delta = norm.ppf((confidence+1)/2.)
+    # Compute value of unbiased variance
+    s = np.sqrt(np.sum(np.square(seq-mean))/(m-1))
+    # Return the confidence interval
+    return ( mean - delta*s/np.sqrt(m), mean + delta*s/np.sqrt(m))
 
-fig, ax = plt.subplots(1,2, figsize=(20,5))
+M = [100, 1000, 10000, 100000]
 
-print("Plotting graphs....")
-
-ax[0].hist(seq, bins=20)
-ax[0].set_xlabel('Range')
-ax[0].set_ylabel('Frequency')
-ax[0].set_title('Histogram of sample')
-
-x = np.arange(0, 1, 0.01)
-y = f(x)
-
-ax[1].plot(x, y, '-b')
-ax[1].set_xlabel('Range')
-ax[1].set_ylabel('Frequency')
-ax[1].set_title('Plot of PDF')
-
-# generate sample for c=20,100
-print()
-seq1 = generate_rv(20)
-print()
-seq2 = generate_rv(100)
-
-fig, ax = plt.subplots(1,3, figsize=(30,5))
-
-
-ax[0].hist(seq, bins=100, color='r')
-ax[0].set_xlabel('Range')
-ax[0].set_ylabel('Frequency')
-ax[0].set_title('Histogram for c=10')
-
-ax[1].hist(seq1, bins=100, color='orange')
-ax[1].set_xlabel('Range')
-ax[1].set_ylabel('Frequency')
-ax[1].set_title('Histogram for c=20')
-
-ax[2].hist(seq2, bins=100, color='lime')
-ax[2].set_xlabel('Range')
-ax[2].set_ylabel('Frequency')
-ax[2].set_title('Histogram for c=100')
-
-
-plt.show()
+for m in M:
+    seq = genSample(m)
+    mean = np.sum(seq)/m
+    left, right = confInt(0.95, seq)
+    # print(seq)
+    print(f"Expected mean for sample size of {m} = ", mean)
+    print(f"95% Confidence interval for the sample = ({left}, {right})")
